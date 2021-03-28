94c479389aaeb29cc5d89f7724c3eb664be25b62
gigiali <ali.je@husky.neu.edu>
2021-03-28 00:48:29 -0400

    changed url in api.js

--- web-ui/src/api.js
+++ web-ui/src/api.js
@@ -2,7 +2,7 @@ import store from './store';
 
 async function api_get(path) {
   let text = await fetch(
-    "http://localhost:4000/api/v1" + path, {});
+    "http://events-spa.jehaneali.site/api/v1" + path, {});
   let resp = await text.json();
   return resp.data;
 }
@@ -16,7 +16,7 @@ async function api_post(path, data) {
     body: JSON.stringify(data),
   };
   let text = await fetch(
-    "http://localhost:4000/api/v1" + path, opts);
+    "http://events-spa.jehaneali.site/api/v1" + path, opts);
   return await text.json();
 }
 
